e51bbb1d9f38ea5ef9652cf55274cc00fc6adf0c
Haeun-Y <hahaha329@ewhain.net>
2021-11-05 03:34:10 +0900

    Update 10825.cpp

--- 10825.cpp
+++ 10825.cpp
@@ -14,40 +14,34 @@ typedef struct student
 }student;
 bool compare(student a, student b)
 {
-	if (a.koreanScore == b.koreanScore)
-	{
-		if (a.englishScore == b.englishScore)
-		{
-			if (a.mathScore == b.mathScore)//모든 점수가 같은 경우
-			{
-				return ((a.name.compare(b.name)) < 0) ? true : false;
-				//a b 순서 일 때 -1 나오고 b a순서일때 양수 나옴 a.namecompare
-			}
-			return a.mathScore > b.mathScore;
-		}
+	if (a.koreanScore != b.koreanScore)
+		return a.koreanScore > b.koreanScore;
 
-		else return a.englishScore < b.englishScore;
-	}
-	else return a.koreanScore > b.koreanScore;
+	if (a.englishScore != b.englishScore)
+		return a.englishScore < b.englishScore;
+
+	if (a.mathScore != b.mathScore)
+		return a.mathScore > b.mathScore;
+
+	return ((a.name.compare(b.name)) < 0) ? true : false;
 
 }
 int main(void)
 {
 	int studentNum = 0;
-	cin >> studentNum;
-	cin.ignore();
+	scanf_s("%d", &studentNum);
 
-	vector<student> v;
+	vector<student> v(studentNum);
+
+	for (int i = 0; i < studentNum; i++)
+		cin >> v[i].name >> v[i].koreanScore >> v[i].englishScore >> v[i].mathScore;
+
+	sort(v.begin(), v.end(), compare);
+
 	for (int i = 0; i < studentNum; i++)
 	{
-		student tmp;
-		cin >> tmp.name >> tmp.koreanScore >> tmp.englishScore >> tmp.mathScore;
-		v.push_back(tmp);
+		cout << v[i].name;
+		printf("\n");
 	}
-	
-	sort(v.begin(), v.end(), compare);
-	
-	for (int i = 0; i < studentNum; i++)
-		cout << v.at(i).name << endl;
 
 }
