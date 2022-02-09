47a0d7b5ca5e2c4e56e0908cdcf910a8988480c9
Haeun-Y <70390323+Haeun-Y@users.noreply.github.com>
2022-02-10 03:31:14 +0900

    Update 1475.cpp

--- 1475.cpp
+++ 1475.cpp
@@ -15,23 +15,19 @@ int calculateMax(vector<int> v)
 int calculateSetNum(string str)
 {
 	vector<int> v(10, 0);
-	
+
 	for (int i = 0; i < str.length(); i++)
 	{
 		int target = str[i] - '0';
 		v[target]++;
 	}
-	int tmp = v[6] + v[9];
-	if (tmp % 2 == 0)//Â¦¼ö
-		v[6] = v[9] = tmp / 2;
-	else//È¦¼ö
-	{
-		tmp = (v[6] > v[9]) ? v[6] : v[9];
-		v[6] = v[9] = tmp;
-	}
 
-	int result = calculateMax(v);
-	return result;
+	int tmp = v[6] + v[9];
+	v[6] = tmp / 2;
+	v[9] = tmp - v[6];
+	
+
+	return calculateMax(v);
 
 }
 
@@ -39,7 +35,6 @@ int main(void)
 {
 	string str;
 	cin >> str;
-	int result = calculateSetNum(str);
-	printf("%d", result);
+	printf("%d", calculateSetNum(str));
 
 }
